44a8915beae039e7a0e7b3803038a081850b63ba
zhangchao <zhangchao_jr@dhcc.com.cn>
2018-09-12 18:08:57 +0800

    express 基础开发

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Expression from './components/Expression';
-import Tab from './components/Tab';
-import Tabs from './components/Tabs';
 
 var components = [
-  Expression,
-  Tab,
-  Tabs
+    Expression
 ];
 export default {
-  install(Vue) {
-    components.map(function(component) {
-      Vue.component(component.name, component);
-    });
+    install(Vue) {
+        components.map(function(component) {
+            Vue.component(component.name, component);
+        });
     },
 };
-export { Expression,Tab,Tabs};
+export { Expression};
 
 
 
